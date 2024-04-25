0b647f0a2c59b22ab99265959bcbc6de8e5f8961
Ttan-24 <tanviprakash24@gmail.com>
2024-04-25 16:03:18 -0400

    Successfully implemented the chart.js and displayed a line chart

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,76 @@
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonImg, IonInput, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import Chart from 'chart.js/auto';
+import { useRef, useEffect } from 'react';
+
+
 
 const Tab2: React.FC = () => {
+  
+  const lineCanvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    lineChartMethod();
+    return () => {
+      destroyChart();
+    };
+  }, []);
+
+    
+  
+  const lineChartMethod = () => {
+    if (!lineCanvasRef.current) {
+      return;
+    }
+
+    const labels = ['1 miles', '2 miles', '3 miles', '4 miles', '5 miles', '6 miles', '7 miles', '8 miles', '9 miles', '10 miles', '11 miles'];
+    const data = [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15];
+
+    var myChart = new Chart(lineCanvasRef.current, {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: 'Elevation Found',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data,
+            spanGaps: false,
+          },
+        ],
+      },
+    });
+    return () => {
+      myChart.destroy()
+    }
+    
+  };
+  
+  const destroyChart = () => {
+    if (lineCanvasRef.current) {
+      const chartInstance = Chart.getChart(lineCanvasRef.current);
+      if (chartInstance) {
+        chartInstance.destroy();
+      }
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -48,7 +116,7 @@ const Tab2: React.FC = () => {
           <IonCardTitle>Elevation Chart</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
-            Sunny skies and warm winds means you should not wear a coat today. However! The terrain of your journey has high elevations, so it is advised you wear a jacket for the coming journey.
+            <canvas ref={lineCanvasRef} style={{ position: 'relative', height: '20vh', width: '40vw' }}></canvas>
           </IonCardContent>
         </IonCard>
       </IonContent>
